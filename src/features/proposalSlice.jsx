3dca1fc5b4a05a50dbe65365fc7edc53ce27fdbb
VamsiKurapati <vamsikrishna.k20@iiits.in>
2025-06-11 19:21:31 +0530

    API issue fixed

--- src/features/proposalSlice.jsx
+++ src/features/proposalSlice.jsx
@@ -7,7 +7,7 @@ export const fetchProposals = createAsyncThunk(
   'proposals/fetchProposals',
   async (_, thunkAPI) => {
     try {
-      const res = await axios.post('http://localhost:5000/api/proposals',{
+      const res = await axios.post('https://proposal-form-backend.vercel.app/api/proposals',{
         name: 'Jane Doe',
         email: 'test@gmail.com',
       });
