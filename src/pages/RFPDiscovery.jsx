d62dec70e573f95988b0ca3178321836976a0843
VamsiKurapati <vamsikrishna.k20@iiits.in>
2025-06-24 21:38:26 +0530

    fix updated

--- src/pages/RFPDiscovery.jsx
+++ src/pages/RFPDiscovery.jsx
@@ -304,7 +304,7 @@ const DiscoverRFPs = () => {
           <MdOutlineCalendarMonth className="text-[16px] text-[#4B5563]" /> Deadline: {rfp.deadline}
         </div>
         <div className="flex items-center gap-2">
-          <MdOutlineAccountBalance className="text-[16px] text-[#4B5563]" /> <p> className="truncate" {rfp.organization} </p>
+          <MdOutlineAccountBalance className="text-[16px] text-[#4B5563]" /> <p className="truncate"> {rfp.organization} </p>
         </div>
       </div>
       <div className="flex justify-between items-center mt-4">
