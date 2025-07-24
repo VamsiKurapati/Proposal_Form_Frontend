1b20d9682a0acfeb64223b1f0b59d1fc541f8872
Vamsi-DraconX <vamsi@draconx.com>
2025-07-24 18:45:41 +0530

    feehb

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,18 +1,11 @@
-import { createContext, useContext, useState, useEffect } from 'react';
+import { createContext, useContext, useState } from 'react';
 
 export const UserContext = createContext();
 
 export const useUser = () => useContext(UserContext);
 
 export const UserProvider = ({ children }) => {
-    const [role, setRole] = useState(localStorage.getItem("role"));
-
-    useEffect(() => {
-        const role = localStorage.getItem("role");
-        if (role) {
-            setRole(role);
-        }
-    }, []);
+    const [role, setRole] = useState("");
 
     return <UserContext.Provider value={{ role, setRole }}>{children}</UserContext.Provider>;
 };
