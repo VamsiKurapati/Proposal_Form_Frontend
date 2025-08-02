ff8b5dc66b8f955c42d758fb5acbd881ff8c9d0b
Vamsi-DraconX <vamsi@draconx.com>
2025-08-02 20:01:22 +0530

    Change

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,11 @@ const App = () => {
   console.log("role : ", role);
   return (
     <>
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={
+        <div className="flex items-center justify-center h-screen">
+          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+        </div>
+      }>
         <Routes>
           <Route path="/" element={role === null ? <Home /> : <Dashboard />} />
 
