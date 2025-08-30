bff4adc2d345278c8e3eb8b95267eb465201616a
Vamsi-DraconX <vamsi@draconx.com>
2025-08-30 22:24:26 +0530

    navigate fix

--- src/components/Subscription.jsx
+++ src/components/Subscription.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { FaRegCheckCircle } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
 
 const baseUrl = "https://proposal-form-backend.vercel.app";
 
 export default function Subscription({ plan }) {
+  const navigate = useNavigate();
   const [plans, setPlans] = useState([]);
   const [isYearlyb, setIsYearlyb] = useState(false);
   const [isYearlyp, setIsYearlyp] = useState(false);
