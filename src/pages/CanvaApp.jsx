bb4492a006c0bd258fa5eb46fded38f5b5feae20
Vamsi-DraconX <vamsi@draconx.com>
2025-09-05 00:32:16 +0530

    change

--- src/pages/CanvaApp.jsx
+++ src/pages/CanvaApp.jsx
@@ -970,7 +970,7 @@ const CanvaApp = () => {
 
       //Continue to basic if subscription is basic and advanced if subscription is pro or enterprise and show a banner to upgrade to advanced if subscription is basic
       const subscription = localStorage.getItem('subscription');
-      if (subscription.plan_name === 'Basic') {
+      if (subscription.plan_name === 'Basic' || subscription.plan_name === 'None') {
         const res = await axios.post(`https://proposal-form-backend.vercel.app/api/proposals/basicComplianceCheck`, {
           proposalId,
           jsonData,
@@ -991,7 +991,7 @@ const CanvaApp = () => {
             text: 'Failed to continue',
           });
         }
-      } else if (subscription.plan_name === 'Pro' || subscription.plan_name === 'Enterprise') {
+      } else if (subscription.plan_name === 'Pro' || subscription.plan_name === 'Enterprise' || subscription.plan_name === 'None') {
         const res = await axios.post(`https://proposal-form-backend.vercel.app/api/proposals/advancedComplianceCheck`, {
           proposalId,
           jsonData,
